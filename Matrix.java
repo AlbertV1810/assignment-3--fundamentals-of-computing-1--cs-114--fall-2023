82abe0c2af2ae5a8149e3853ce930cba8208e3d4
AlbertV1810 <alvargas@hartford.edu>
2023-11-14 23:11:38 -0500

    moved methods to matrix

--- Matrix.java
+++ Matrix.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
 public class Matrix {
-  public static void main(String[] args){
-    RunMatrix.printMatrix();
+  Scanner input = new Scanner(System.in);
+  int arraySize = input.nextInt();
+  int[][] inputMatrix = new int[arraySize][arraySize];
+  int row;
+  int column;
+  int matrixValue = 1;
+
+  public static void printMatrix(){
+    System.out.print("Please enter a positive number for the size of your matrix: ");
+    Scanner input = new Scanner(System.in);
+    int arraySize = input.nextInt();
+    int[][] inputMatrix = new int[arraySize][arraySize];
+    int row;
+    int column;
+
+    System.out.println("Your matrix is " + arraySize + "x" + arraySize + "\n" );
+    System.out.println("Printing your matrix with default values: " + "\n");
+    for(row = 0; row < arraySize; row++){ //Prints non populated matrix
+      for(column = 0; column < arraySize; column++){
+        System.out.print(inputMatrix[row][column] + "\t");
+      }
+      System.out.println();
+    }
+    input.close();
   }
-
-
-
-
+  public void populateMatrix(){
+    System.out.println("Populating Matrix... ");
+    System.out.println("Printing populated Matrix: ");
+    for(row = 0; row < arraySize; row++){ //Prints populated matrix
+      for(column = 0; column < arraySize; column++){
+        inputMatrix[row][column] = matrixValue++; //Gives a value to each place in the matrix
+        System.out.print(inputMatrix[row][column] + "\t");
+        }
+       System.out.println();
+    }
+    System.out.println();
+    input.close();
+  }
 }
