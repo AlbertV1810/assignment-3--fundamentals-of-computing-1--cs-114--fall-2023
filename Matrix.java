7b2aac4f5ad4a09b2d695a74ac9aebf238fa1fc6
AlbertV1810 <alvargas@hartford.edu>
2023-11-15 20:49:59 -0500

    attempt at flipMatrix

--- Matrix.java
+++ Matrix.java
@@ -3,9 +3,11 @@ public class Matrix {
   static Scanner input = new Scanner(System.in);
   static int arraySize = input.nextInt();
   static int[][] inputMatrix = new int[arraySize][arraySize];
-  static int row, column, hold, rowOne, columnOne, rowTwo, columnTwo;
+  static int row, column, x1, y1, x2, y2;
   static int matrixValue = 1;
 
+  public static final String ANSI_YELLOW = "\u001B[33m";
+  public static final String ANSI_RESET = "\u001B[0m";
   public static void printMatrix(){
     System.out.println("Your matrix is " + arraySize + "x" + arraySize + "\n" );
     System.out.println("Printing your matrix with default values: " + "\n");
@@ -17,8 +19,12 @@ public class Matrix {
     }
     input.close();
   }
-  public static final String ANSI_YELLOW = "\u001B[33m";
-  public static final String ANSI_RESET = "\u001B[0m";
+
+  private static void swap() {
+    int hold = inputMatrix[x1][y1];
+    inputMatrix[x1][y1] = inputMatrix[x2][y2];
+    inputMatrix[x2][y2] = hold;
+  }
 
   public static void populateMatrix(){
     System.out.println("Populating Matrix... ");
@@ -26,16 +32,18 @@ public class Matrix {
     for(row = 0; row < arraySize; row++){ //Prints populated matrix
       for(column = 0; column < arraySize; column++){
         inputMatrix[row][column] = matrixValue++; //Gives a value to each place in the matrix
-        System.out.print(ANSI_YELLOW + inputMatrix[row][column] + "\t" + ANSI_RESET);
+        System.out.print(inputMatrix[row][column] + "\t");
         }
        System.out.println();
     }
     System.out.println();
   }
-  public static void swap(){
-    hold = inputMatrix[rowTwo][columnTwo];
-    inputMatrix[rowTwo][columnTwo] = inputMatrix[rowOne][columnOne];
-    inputMatrix[rowOne][columnOne] = inputMatrix[rowTwo][columnTwo];
-
+  public static void flippedMatrix(){
+  int [][] flippedMatrix = new int[arraySize][arraySize];
+  for (int row = 0; row < arraySize; row++) {
+    for (int column = 0; column < arraySize; column++) {
+      flippedMatrix[row][column] = inputMatrix[row][column];
+      }
+  }
   }
 }
